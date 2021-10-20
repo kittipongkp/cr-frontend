97d451905cd3f2b6f688d1ff94b0384a8fec7c9a
kittipongkp <kittipongga@gmail.com>
2021-10-20 23:41:59 +0700

    End EP5

---
+++ src/components/CoursesItems.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Courses } from '../interfaces/Courses'
+
+type CoursesItemProps = {
+    course: Courses
+}
+
+ const CoursesItems = (props: CoursesItemProps) => {
+     let courses = props.course
+    return (
+        <li key={courses.id}>
+            {courses.number} - {courses.title}
+        </li>
+    )
+}
+
+export default  CoursesItems
