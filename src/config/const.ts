743d7fdba6be4721900bfcee9fbaf29e36233bb9
kittipongkp <kittipongga@gmail.com>
2021-11-06 09:08:06 +0700

    fix

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,1 +1,4 @@
-export const baseUrl = (process.env.NODE_ENV === "development")? "http://localhost:5000": "http://123.12.11.1:5000"
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "http://123.12.11.1:5000";
